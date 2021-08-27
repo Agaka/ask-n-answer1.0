8c426a250de542ab41d7dcb1d4be455bc5194423
Allan Gabriel Sobrero dos Santos <55900557+lAgakal@users.noreply.github.com>
2021-08-27 09:11:50 -0300

    main

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const Pergunta = require("./database/Pergunta")
+const Resposta = require("./database/Resposta")
+//Database
+
+connection.authenticate().then(() => {
+    console.log("Conectado com Banco de dados")
+}).catch((err) => {
+    console.log(err)
+})
+
+//EJS
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+
+//BodyParser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+    Pergunta.findAll({ raw: true, order: [
+        ['id', 'DESC']
+    ]}).then((perguntas) => {
+        res.render("index", {
+            perguntas: perguntas
+        })
+    })
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/")
+    })
+})
+
+app.get("/pergunta/:id", (req, res) => {
+    let id = req.params.id
+
+    Pergunta.findOne({
+        where: { id: id }
+    }).then((pergunta) => {
+        if(pergunta != undefined) {
+
+            Resposta.findAll({
+                where: { perguntaId: id }
+            }).then((respostas) => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                })
+            })
+            
+        } else {
+            res.redirect("/")
+        }
+    })
+})
+
+app.post("/responder", (req, res) => {
+    let corpo = req.body.corpo
+    let perguntaId = req.body.pergunta
+
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId)
+    })
+})
+
+app.listen(3000, () => {
+    console.log("App Rodando")
+})
